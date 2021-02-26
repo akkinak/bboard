e19725266bb1baa25a6b68541340a80cbcded81c
akkinak <akkinak7@gmail.com>
2021-02-26 18:59:34 +0200

    1a

---
+++ samplesite/bboard/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Bb
+from .models import Rubric
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+    list_display = ('title','content', 'price', 'published', 'rubric')
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
+
+# Register your models here.
---
+++ samplesite/bboard/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True,
+    verbose_name = 'Опис')
+    price = models.FloatField(null=True, blank=True, verbose_name='Ціна')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+    verbose_name = 'Опyбліковано')
+    rubric = models.ForeignKey('Rubric', null=True,
+    on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    class Meta:
+        verbose_name_plural = 'Оголошення'
+        verbose_name = 'Оголошення'
+        ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True,
+    verbose_name='Назва')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
+
+# Create your models here.
